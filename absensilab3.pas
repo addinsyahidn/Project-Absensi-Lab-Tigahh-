25937df5effb139d24fb6a36e0ff3b087f035634
addinsyahidn <addinsyahidn@gmail.com>
2025-12-05 19:30:21 +0700

    Update absensilab3.pas

--- absensilab3.pas
+++ absensilab3.pas
@@ -1,11 +1,18 @@
 program absensilab3;
-uses crt;
+uses crt, sysutils;
 
 type
   data : record
     nama : string[30];
     nim : string[9];
+    keaktifan : 0..14;
+    absen : 0..14;
+    izin : 0..14;
+    sakit : 0..14;
   end;
+
+var
+  absensi : array[1..22] of data;
 
 //Fungsi
 
